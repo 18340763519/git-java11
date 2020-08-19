77837205b093fc24d933536e66f82490e1ea9e4e
18340763519 <69193039+18340763519@users.noreply.github.com>
2020-08-19 16:26:22 +0800

    Update Hello.java
    修改

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -5,5 +5,8 @@ public static void main(String[] args) {
 	System.out.println();
 	System.out.println("宇");
 	System.out.println("宇3");
+	System.out.println(123);
+	System.out.println("宇456");
+	System.out.println("宇3789");
 }
 }
