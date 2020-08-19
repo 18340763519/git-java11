6c017efc577b43e21d5437fdce1855da19f7629c
18340763519 <2389785274@qq.com>
2020-08-19 16:27:27 +0800

    123456

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 public static void main(String[] args) {
 	System.out.println();
 	System.out.println("宇");
-	System.out.println("宇3");
+	System.out.println("宇7788");
+	System.out.println(456456);
+	System.out.println("5555555");
+	System.out.println("2222333");
 }
 }
